a5fc37efacaa5417cf199ea6d9c4d2645e140d52
Rox <you@example.com>
2021-11-03 20:29:46 +0100

    harley-quinn

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hola mundo')
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
